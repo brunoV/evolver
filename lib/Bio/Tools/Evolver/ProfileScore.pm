a17d06b620559859468486d60bd06d25700f189d
Bruno Vecchi <vecchi.b@gmail.com>
2009-02-20 11:32:16 -0200

    first draft of the new, simple ProfileScore function

--- lib/Bio/Tools/Evolver/ProfileScore.pm
+++ lib/Bio/Tools/Evolver/ProfileScore.pm
@@ -2,25 +2,28 @@ package Bio::Tools::Evolver::ProfileScore;
 use strict;
 use warnings;
 use Moose::Role;
+use Moose::Util::TypeConstraints;
 
-use Bio::Tools::Run::Alignment::Clustalw;
-use Bio::AlignIO;
+use List::MoreUtils qw(each_array);
+use Bio::Matrix::IO;
 
-use List::Util qw(shuffle);
-use Memoize;
-memoize('_score_f_absolute');
-
-has _random_seq => (
-   is         => 'ro',
-   lazy_build => 1,
-   isa        => 'Str',
+has 'matrix' => (
+   is      => 'ro',
+   isa     => 'BTE.Bio.Matrix.Score',
+   coerce  => 1,
+   default => 'BLOSUM62',
 );
 
-sub _build__random_seq {
-   my $self       = shift;
-   my $random_seq = _shuffle_string(
-      'ACDEFGHIKLMNPQRSTVWY' x int( $self->profile->length / 20 ) );
-   return $random_seq;
+coerce 'BTE.Bio.Matrix.Score'
+   => from 'BTE.MatrixFile' => via { _parse_matrixfile( $_[0] ) }
+   => from 'BTE.Matrix.IO' =>  via { return $_[0]->next_matrix };
+#   => from 'BTE.MatrixName' => via { _parse_matrixfile( '../../../../etc/' . $_[0] ) };
+
+sub _parse_matrixfile {
+   my $file = shift;
+   my $parser = Bio::Matrix::IO->new( -file => $file )
+       or die "Couldn't open scoring matrix $file : $!";
+   return $parser->next_matrix;
 }
 
 has _my_fitness => (
@@ -32,45 +35,36 @@ has _my_fitness => (
 sub _build__my_fitness {
    my $self = shift;
 
-   my $factory = Bio::Tools::Run::Alignment::Clustalw->new( quiet => 1 );
-
+   my $consensus = $self->profile->consensus_string;
    # Given a string, calculate the "family belongness score".
-   my $max_score = _score_f_absolute( $self->profile->consensus_string,
-      $self->profile, $factory );
-   my $min_score = _score_f_absolute( $self->_random_seq, $self->profile,
-      $factory );
-
-   my $prof_file = $self->profile;
+   my $max_score = $self->_score_f_absolute( $consensus,
+      $consensus );
+   my $min_score = -4 * length $consensus;
 
    return sub {
       my $string = shift;
       my $string_score 
-         = _score_f_absolute( $string, $prof_file, $factory );
+         = $self->_score_f_absolute( $string,  $consensus );
       my $score = ( ( $string_score - $min_score ) / ( $max_score - $min_score ) );
       if ($score < 0) { $score = 0 };
       return $score;
    };
 }
 
-sub _shuffle_string {
-   my $string = shift;
-   my @elms = split '', $string;
-   return join( '', shuffle @elms );
-}
-
 sub _score_f_absolute {
 
    # Given a string, calculate the alignment score against the given
    # profile.
-   my ( $seq_string, $prof_file, $factory ) = @_;
-   my $alignment = $factory->profile_align(
-      $prof_file,
-      Bio::Seq->new(
-         -id  => 'x',
-         -seq => $seq_string,
-      )
-   );
-   return $alignment->score;
+   my ( $self, $seq_string, $cons_string ) = @_;
+   my $score;
+   my @seq_res = split '', $seq_string;
+   my @cons_res = split '', $cons_string;
+
+   my $pair = each_array( @seq_res, @cons_res );
+   while ( my ($seq, $cons) = $pair->() ) {
+      $score += $self->matrix->get_entry($seq, $cons);
+   }
+   return $score;
 }
 
 no Moose;
