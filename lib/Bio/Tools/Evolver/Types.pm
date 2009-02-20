3e73cb4a94e7e3d8290c8ebd8b841beb517da021
Bruno Vecchi <vecchi.b@gmail.com>
2009-02-20 11:42:00 -0200

    Added matrix types to Types.pm

--- lib/Bio/Tools/Evolver/Types.pm
+++ lib/Bio/Tools/Evolver/Types.pm
@@ -20,8 +20,34 @@ subtype 'BTE.Bio.SimpleAlign'  => as class_type('Bio::SimpleAlign');
 subtype 'BTE.Probability'      => as 'Str' => where { $_ < 1 and $_ > 0 };
 
 sub validate_aln {
-   # Uninplemented
-   return 1
+
+   # Uninplemented. I'm planning on analyzing the file's extensions
+   # and try to throw exceptions if they are not recognized. The problem
+   # arises with formats such as FASTA, that can be both an alignment
+   # or a sequence ¿default to sequence? I guess so, in the worst case
+   # it will realign it; if the user really wants to keep their alignment
+   # intact, save it in a better format, damn it.
+   return 1;
+}
+
+# Scoring Matrix Types
+subtype 'BTE.Bio.Matrix.Scoring' => as class_type('Bio::Matrix::Scoring');
+subtype 'BTE.MatrixFile'
+   => as 'Str'
+   => where { validate_mfile($_) }
+   => message {"File $_ doesn't exist"};
+subtype 'BTE.Bio.Matrix.IO' => as class_type('Bio::Matrix::IO');
+subtype 'BTE.MatrixName' => 'Str' => where { validate_mname($_) };
+subtype 'BTE.Algorithm.NeedlemanWunsch'
+   => as class_type('Algorithm::NeedlemanWunsch');
+
+
+sub validate_mfile { return 1 if -e $_[0] }
+
+sub validate_mname {
+   my $name     = shift;
+   my @matrixes = qw(BLOSUM62 BLOSUM80 BLOSUM90 BLOSUM45 BLOSUM50);
+   return grep { $name eq $_ } @matrixes;
 }
 
 no Moose;
