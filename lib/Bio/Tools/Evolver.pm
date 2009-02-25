6d4cebb63d135f7583322d9488ca5e0031e61443
bruno <bruno@marta.(none)>
2009-02-25 16:18:35 -0200

    some TODO list

--- lib/Bio/Tools/Evolver.pm
+++ lib/Bio/Tools/Evolver.pm
@@ -231,6 +231,15 @@ sub getFittest {
 no Moose;
 __PACKAGE__->meta->make_immutable;
 1;    # End of Bio::Tools::Evolver
+# TODO
+# inject consensus
+# eliminate hardcoding in the weighing of both scores.
+# design a better way to implement the profile score engine. Maybe an
+# interface ProfileScoreI than can e
+# Maybe a better charting role, using Chart::Clicker?
+# Extend for different sequence alphabets:
+#  * $proth_alph
+#  * _init
 __END__
 
 =head1 NAME
