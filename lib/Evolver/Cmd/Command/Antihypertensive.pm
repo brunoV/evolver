a39a2972ea94354ca1406d9153b479554ff16e60
brunov <vecchi.b@gmail.com>
2009-10-12 23:00:32 -0300

    Rename ::Command::ACE to ::Command::Antihypertensive

--- lib/Evolver/Cmd/Command/Antihypertensive.pm
+++ lib/Evolver/Cmd/Command/Antihypertensive.pm
@@ -1,4 +1,4 @@
-package Evolver::Cmd::Command::ACE;
+package Evolver::Cmd::Command::Antihypertensive;
 use Moose;
 
 extends 'Evolver::Cmd::Biopep';
@@ -10,3 +10,11 @@ use MooseX::Types::Path::Class qw(File);
 has '+activity' => ( default => 'antihypertensive' );
 
 __PACKAGE__->meta->make_immutable;
+
+=pod
+
+=head1 NAME
+
+Evolver::Cmd::Command::Antihypertensive - Increase the amount of encrypted antihypertensive peptides
+
+=cut
