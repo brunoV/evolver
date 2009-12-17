a8cb179f780e259accf1b41389a547a24815e8cb
bruno <bruno@lilita.(none)>
2009-12-17 18:39:41 -0300

    added assembly_function.name to the Result view

--- lib/Evolver/DB/Result/Result.pm
+++ lib/Evolver/DB/Result/Result.pm
@@ -9,11 +9,20 @@ use base 'Evolver::DB::Result';
 CLASS->table_class('DBIx::Class::ResultSource::View');
 
 CLASS->table('result');
+
 CLASS->result_source_instance->view_definition(
-"select r.id, f.name as fitness, r.profile_algorithm, r.inject_consensus, r.mutation, r.crossover, r.strategy, r.parents,
-r.selection, r.preserve, r.population_size, r.generation as max_generation, r.history, r.description as run_description, s.seq, o.custom_score,
-o.total_score, o.generation, o.name, r.mutation * r.population_size * o.generation as cost from run r inner join fitness f on f.id = r.fitness_id
-inner join optimized_seq o on o.run_id = r.id inner join result_seq s on o.seq_id = s.id;"
+
+    "select r.id, f.name as fitness, a.name as assembly_function,
+    r.profile_algorithm, r.inject_consensus, r.mutation, r.crossover,
+    r.strategy, r.parents, r.selection, r.preserve, r.population_size,
+    r.generation as max_generation, r.history, r.description as
+    run_description, s.seq, o.custom_score, o.total_score, o.generation,
+    o.name, r.mutation * r.population_size * o.generation as cost from
+    run r inner join fitness f on f.id = r.fitness_id inner join
+    assembly_function a on a.id = r.assembly_function_id inner join
+    optimized_seq o on o.run_id = r.id inner join result_seq s on
+    o.seq_id = s.id;"
+
 );
 
 CLASS->add_columns(
@@ -25,6 +34,13 @@ CLASS->add_columns(
     size => undef,
   },
   "fitness",
+  {
+    data_type => "varchar",
+    default_value => undef,
+    is_nullable => 0,
+    size => undef,
+  },
+  "assembly_function",
   {
     data_type => "varchar",
     default_value => undef,
